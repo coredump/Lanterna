19e05f9ab824b1ffedb2a020ed5de9d4612d8702
coredump <jose.junior@gmail.com>
2009-05-08 13:07:27 -0300

    License headers, GPL v3

--- src/br/eti/core/lanterna/ImageStrings.java
+++ src/br/eti/core/lanterna/ImageStrings.java
@@ -1,5 +1,24 @@
 package br.eti.core.lanterna;
 
+/*
+ * Lanterna - Copyright 2009 José de Paula E. Junior (jose.junior@gmail.com)
+ * 
+ * This file is part of Lanterna.
+
+ * Lanterna is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Lanterna is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Lanterna.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 public class ImageStrings {
 
 	public static final String HEART = "000111110000000011111000001111111000000111111100011111111100001111111110011111111100001111111110111111111110011111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111011111111111111111111110011111111111111111111110001111111111111111111100001111111111111111111100000111111111111111111000000011111111111111110000000001111111111111100000000000111111111111000000000000011111111110000000000000001111111100000000000000000111111000000000000000000011110000000000000000000001100000000000";
